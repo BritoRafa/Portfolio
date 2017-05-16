12a47e22aef58e0ca8058ae9f237a5fc74a4d566
BritoRafa <rafaelbritodesign@gmail.com>
2017-05-16 16:03:17 -0300

    Adicionado detector de Cruzamento
    Foi adicionado um Detector para verificar quando o inimigo chega no
    ponto final e retirar uma vida do jogador

---
+++ Tower Defense/Assets/Scripts/DetectorDeCruzamento.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DetectorDeCruzamento : MonoBehaviour {
+	[SerializeField] private Jogador jogador;
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.CompareTag ("Inimigo")) 
+		{
+			Debug.Log ("Chegou no fim do caminho");
+			Destroy (collider.gameObject);
+			jogador.PerdeVida ();
+		}
+	}
+}
