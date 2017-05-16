67c95513243fb0bb9da30e14cf554cce4bf047f9
BritoRafa <rafaelbritodesign@gmail.com>
2017-05-16 16:01:29 -0300

    Adicionado Mostrador de Vida

---
+++ Tower Defense/Assets/Scripts/MostradorDeVida.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MostradorDeVida : MonoBehaviour {
+
+	private Text CampoTexto;
+	[SerializeField]private Jogador jogador;
+	void Start () 
+	{
+		CampoTexto = GetComponent<Text> ();
+	}
+	
+
+	void Update () 
+	{
+		CampoTexto.text = "Vida: " + jogador.GetVida ();
+	}
+}
