2e38d32b468e8ffa512651900fc60703df7cfccf
BritoRafa <rafaelbritodesign@gmail.com>
2017-05-16 16:05:30 -0300

    Adicionado torre com metralhadora
    Foi adicionado um segundo tipo de torre com uma taxa de disparos maior
    que a primeira e um custo de créditos maior

---
+++ Tower Defense/Assets/Scripts/TorreMetralhadora.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorreMetralhadora : Torre {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		alvo = EscolheAlvo();
+		if (alvo != null) 
+		{
+			Atira (alvo);
+		}
+	}
+	private void Atira(Inimigo inimigo)
+	{
+		float TempoAtual = Time.time;
+		if (TempoAtual > MomentoDoUltimoDisparo + TempoDeRecarga) 
+		{
+			MomentoDoUltimoDisparo = TempoAtual;
+			GameObject PontoDeDisparo1 = this.transform.Find ("CanhoesDaTorre/MetralhaDir/PontoDeDisparoDir").gameObject;
+			GameObject PontoDeDisparo2 = this.transform.Find ("CanhoesDaTorre/MetralhaEsq/PontoDeDisparoEsq").gameObject;
+			Vector3 PosicaoDoPontoDeDisparo1 = PontoDeDisparo1.transform.position;
+			Vector3 PosicaoDoPontoDeDisparo2 = PontoDeDisparo2.transform.position;
+			GameObject ProjetilObject = (GameObject)Instantiate (projetilPrefab, PosicaoDoPontoDeDisparo1, Quaternion.identity);
+			GameObject ProjetilObject2 = (GameObject)Instantiate (projetilPrefab, PosicaoDoPontoDeDisparo2, Quaternion.identity);
+			Missil missil = ProjetilObject.GetComponent<Missil> ();
+			missil.DefineAlvo (inimigo);
+			missil = ProjetilObject2.GetComponent<Missil> ();
+			missil.DefineAlvo (inimigo);
+		}
+	}
+
+
+}
