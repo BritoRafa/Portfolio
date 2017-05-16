6a51e8c918d594a0093ffbb69722b50bcc250f4f
BritoRafa <rafaelbritodesign@gmail.com>
2017-05-16 16:02:06 -0300

    Adicdionado mostrador de Créditos
    Foi adicionado um mostrador de Créditos para o jogar ter que gasta los
    toda vez que for criar uma nova torre

---
+++ Tower Defense/Assets/Scripts/MostradorDeCreditos.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MostradorDeCreditos : MonoBehaviour {
+
+	private Text CampoTexto;
+	[SerializeField]private Jogador jogador;
+	void Start () {
+		CampoTexto = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CampoTexto.text = "Créditos: " + jogador.GetCredit();
+	}
+}
