c540e1cbcd9682b15836d7443a10a8a96c32fafc
BritoRafa <rafaelbritodesign@gmail.com>
2017-05-16 16:06:25 -0300

    Adicionado botões para selecionar tipos de torres
    foram adicionados novos botões para o jogador poder escolher qual o tipo
    de torre que ele quer criar

---
+++ Tower Defense/Assets/Scripts/BotaoMetralhadora.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotaoMetralhadora : MonoBehaviour {
+	[SerializeField]private Jogo jogo;
+
+	public void SelecionaTorre()
+	{
+		jogo.GetComponent<Jogo> ();
+		jogo.SetID (1);
+	}
+}
---
+++ Tower Defense/Assets/Scripts/BotaoTorreBasica.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotaoTorreBasica : MonoBehaviour {
+	[SerializeField]private Jogo jogo;
+
+	void Start()
+	{
+		jogo.GetComponent<Jogo> ();
+	}
+	public void SelecionaTorre()
+	{
+		jogo.SetID (0);
+	}
+}
