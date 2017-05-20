cf358501c3eb185da8c3ebc9fe83f51ce2bebd02
Rafael Brito <rafaelbritodesign@gmail.com>
2017-05-19 21:09:32 -0300

    Script Mostrador de Vida revisado

--- Tower Defense/Assets/Scripts/MostradorDeVida.cs
+++ Tower Defense/Assets/Scripts/MostradorDeVida.cs
@@ -6,12 +6,12 @@ public class MostradorDeVida : MonoBehaviour {
 
 	private Text CampoTexto;
 	[SerializeField]private Jogador jogador;
+	//Captura o componente Text do objeto
 	void Start () 
 	{
 		CampoTexto = GetComponent<Text> ();
 	}
-	
-
+	//Define o texto do mostrador de vida
 	void Update () 
 	{
 		CampoTexto.text = "Vida: " + jogador.GetVida ();
