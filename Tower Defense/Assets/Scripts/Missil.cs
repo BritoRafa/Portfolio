2f7b5e170674e78a0d1bdadd6f33b8864d875be8
Rafael Brito <rafaelbritodesign@gmail.com>
2017-05-19 21:20:06 -0300

    Script Missil revisado

--- Tower Defense/Assets/Scripts/Missil.cs
+++ Tower Defense/Assets/Scripts/Missil.cs
@@ -6,12 +6,12 @@ public class Missil : MonoBehaviour {
 	private Inimigo Alvo;
 	[SerializeField] private int Dano;
 
-	// Use this for initialization
+	// Chama o método AutoDestruicao e define o tempo como 2
 	void Start () {
 		AutoDestruicao (2);
 	}
 	
-	// Update is called once per frame
+	// Chama o método AlteraDirecao
 	void Update () {
 		Anda ();
 		if(Alvo != null)
@@ -19,16 +19,21 @@ public class Missil : MonoBehaviour {
 			AlteraDirecao ();
 		}
 	}
+	//Método responsável por fazer o míssil se mover
 	private void Anda()
 	{
 		Vector3 PosicaoAtual = transform.position;
 		Vector3 Deslocamento = transform.forward * Time.deltaTime * Velocidade;
 		transform.position = PosicaoAtual + Deslocamento;
 	}
+	//
+	//Método responsável por definir o alvo do míssil
 	public void DefineAlvo(Inimigo inimigo)
 	{
 		Alvo = inimigo;
 	}
+	//
+	//Método responsável por controlar o míssil em direção ao alvo
 	private void AlteraDirecao()
 	{
 		Vector3 PosicaoAtual = transform.position;
@@ -36,10 +41,8 @@ public class Missil : MonoBehaviour {
 		Vector3 DirecaoDoAlvo = PosicaoDoAlvo - PosicaoAtual;
 		transform.rotation = Quaternion.LookRotation (DirecaoDoAlvo);
 	}
-	private void AutoDestruicao (float segundos)
-	{
-		Destroy (this.gameObject, segundos);
-	}
+	//
+	//Método responsável por causar dano ao objeto inimigo e destruir o objeto míssil quando eles colidirem
 	void OnTriggerEnter (Collider ElementoColidido)
 	{
 		if (ElementoColidido.CompareTag ("Inimigo")) 
@@ -49,5 +52,13 @@ public class Missil : MonoBehaviour {
 			inimigo.RecebeDano (Dano);
 		}
 	}
+	//
+	//Método responsável por destruir automaticamente o míssil após um tempo 
+	//(método criado para destruir misseis que não atingiram nenhum alvo evitando assim o excesso de Gameobjects)
+	private void AutoDestruicao (float segundos)
+	{
+		Destroy (this.gameObject, segundos);
+	}
+	//
 
 }
